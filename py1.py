81a224a48fba7b414e54c1baa6567ae5a1b1c452
lcbjrrr <luiz.junior1@ibmec.edu.br>
2020-07-29 11:00:27 -0300

    ok

--- py1.py
+++ py1.py
@@ -1,6 +1,10 @@
 
 def nota_final(ap1, ap2, ap3):
+    return ap1*0.4 + ap2*0.4 + ap3*0.2
 
 def imc(peso,altura):
+    return peso/(altura*altura)
 
 def FtoC(f):
+  c = (5 * (f-32) / 9)
+  return c
